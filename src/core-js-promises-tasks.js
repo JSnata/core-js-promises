88d91a43ada4e8dd1950f55582b9b1e78d22ca2a
natagetup <n.kulikouskaya@gmail.com>
2024-04-03 18:51:16 +0200

    feat: implement getFirstResolvedPromiseResult task implementation

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -61,8 +61,13 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  promises.forEach((promise, index) => {
+    promise.then(() => {
+      return promises[index];
+    });
+  });
+  return promises[promises.length - 1];
 }
 
 /**
